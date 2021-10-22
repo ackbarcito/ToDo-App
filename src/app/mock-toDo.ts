14723085ee29588d40dabd8bec08053070ee216c
ackbarcito <xsebas9@gmail.com>
2021-10-22 10:17:17 -0300

    changed workspace to Ubuntu & fixed add float button

--- src/app/mock-toDo.ts
+++ src/app/mock-toDo.ts
@@ -1,7 +1,28 @@
 import { ToDo } from './toDo.model';
 
 export const TODOS: ToDo[] = [
-  new ToDo(0, 'Aprender Html', 'Debo aprender html', 'Developer', true, 80),
-  new ToDo(1, 'Aprender Css', 'Debo aprender Css', 'Web', false, 30),
-  new ToDo(2, 'Aprender Typescript', 'Atento a Typescript', 'Web', true, 60),
+  new ToDo(
+    0,
+    'Terminar Tp de Arquitectura',
+    'Leer a partir de la bolilla 6 del libro "Arquitectura. Forma, espacio y orden." Libro de Frank Ching',
+    'Facultad',
+    true,
+    80
+  ),
+  new ToDo(
+    1,
+    'Comprar carne para el asado del domingo',
+    'Comprar en la carneria de la esquina: 1kg de Vacio, 2kg de costilla, 0,5kg de chinculin, 1kg de chorizo criollo y 0,5kg de morcilla.',
+    'Familia',
+    false,
+    30
+  ),
+  new ToDo(
+    2,
+    'Comprar Placa de Video',
+    'Comparar los precios de Placa de video de distintos locales de computacion.',
+    'Personal',
+    true,
+    60
+  ),
 ];
